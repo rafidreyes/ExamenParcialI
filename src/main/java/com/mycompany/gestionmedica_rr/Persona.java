834d57b2ece60263c0ae758bb9de70f36f2bc96e
R4f4Reyes <rdreyes1@uth.hn>
2024-11-23 12:54:33 -0600

    ExamenParcial_II

---
+++ src/main/java/com/mycompany/gestionmedica_rr/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestionmedica_rr;
+
+/**
+ *
+ * @author Rafael Reyes
+ */
+public class Persona {
+    private String nombre_RR;
+    private int edad_RR;
+    private String direccion_RR;
+
+    public Persona(String nombre_RR, int edad_RR, String direccion_RR) {
+        this.nombre_RR = nombre_RR;
+        this.edad_RR = edad_RR;
+        this.direccion_RR = direccion_RR;
+    }
+
+    public String getNombre_RR() {
+        return nombre_RR;
+    }
+
+    public void setNombre_RR(String nombre_RR) {
+        this.nombre_RR = nombre_RR;
+    }
+
+    public int getEdad_RR() {
+        return edad_RR;
+    }
+
+    public void setEdad_RR(int edad_RR) {
+        this.edad_RR = edad_RR;
+    }
+
+    public String getDireccion_RR() {
+        return direccion_RR;
+    }
+
+    public void setDireccion_RR(String direccion_RR) {
+        this.direccion_RR = direccion_RR;
+    }
+    
+    
+    
+}
